6a808444b65d246ed3e6d8f03b0b855f18e16943
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-17 17:18:20 +0100

    First test of uploading to a new repository

---
+++ less1_lists.py
@@ -0,0 +1,5 @@
+# Lists: ordered, mutable, allows duplicate elements
+
+"""Creating a list"""
+mylist = ["banana", "cherry", "apple"]  # inside we put any elements, separated with comma
+print(mylist)  # --> ['banana', 'cherry', 'apple']
